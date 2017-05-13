214764b0235f5a32e89f94b2eb37d973ade3d887
HL0718 <1391716398@qq.com>
2017-05-13 16:48:50 +0800

    Add files via upload
    1

--- step2.py
+++ step2.py
@@ -15,62 +15,52 @@ if __name__ == '__main__':
     )
     #获取数据库执行游标
     cur=conn.cursor()
-    print "select * from department"
-    cur.execute("select * from department")
-
+    print "select * from student"
+    cur.execute("select * from student")
+#
 #将文件department.txt中的数据输入表department中
 
-    with open ("/home/zheng/Download/MySQL上课文件/作业/university/department.txt") as f:
-        while True:
-            line=f.readline()
-            if line:
-                line=line.strip('\n')
-                line = line.split(",")
-                print line
-                dept_name=line[0]
-                building=line[1]
-                budget=line[2]
-                cur.execute("insert into department(dept_name,building,budget) values(%d,'%s',%d)" % (dept_name, building, budget))
-            else:
-                break
-                f.close()
-                cur.close()
+    def prc_line(line):
+        _result = line.decode("utf-8").strip().split(" ")
+        _result[0] = int(_result[0])
+        _result[2] = int(_result[2])
+        return _result
+
+
+    with open("/home/zheng/Download/MySQL上课文件/作业/university/department.txt", "r") as f:
+        datas = [prc_line(line) for line in f.readlines()]
+
+    for d in datas:
+                cur.execute("insert into department(dept_name,building,budget) values(%d,'%s',%d)" % (d[0],d[1],d[2]))
+
                 conn.commit()
     #将文件exam.txt中的数据输入到表exam中
-    with open("/home/zheng/Download/MySQL上课文件/作业/university/exam.txt") as f:
-       while True:
-           line=f.readline()
-           if line:
-               line = line.strip('\n')
-               line = line.split(",")
-               print line
-               student_ID=line[0]
-               exam_name=line[1]
-               grade=line[2]
-               cur.execute("insert into exam(student_ID,exam_name,grade) values(%d,'%s',%d)" % (student_ID, exam_name,grade))
-           else:
-               break
-               f.close()
-               cur.close()
-               conn.comit()
+    def prc_line(line):
+        _result = line.decode("utf-8").strip().split(" ")
+        _result[0] = int(_result[0])
+        _result[2] = int(_result[2])
+        return _result
+
+
+    with open("/home/zheng/Download/MySQL上课文件/作业/university/exam.txt","r") as f:
+        datas = [prc_line(line) for line in f.readlines()]
+
+    for d in datas:
+               cur.execute("insert into exam(student_ID,exam_name,grade) values(%d,'%s',%d)" % (d[0],d[1],d[2]))
+
+               conn.commit()
+
     #将文件student.txt中的数据输入表student中
-    with open("/home/zheng/Download/MySQL上课文件/作业/university/student.txt") as f:
-        while True:
-            line=f.readline()
-            if line:
-                line=line.strip('\n')
-                line=line.spilt(",")
-                print line
-                ID=line[0]
-                name=line[1]
-                sex=line[2]
-                age=line[3]
-                emotion_state=line[4]
-                dept_name=line[5]
-                cur.execute("insert into student(ID,name,sex,age,emotion_state,dept_name) values(%d,'%s','%s',%d,'%s','%s')" % (ID, name, sex,age,emotion_state,dept_name))
-            else:
-                break
-                f.close()
-                cur.close()
-                conn.comit()
-                conn.close()
+               def prc_line(line):
+                   _result = line.decode("utf-8").strip().split(" ")
+                   _result[0] = int(_result[0])
+                   _result[3] = int(_result[3])
+                   return _result
+
+
+               with open("/home/zheng/Download/MySQL上课文件/作业/university/student.txt","r") as f:
+                   datas = [prc_line(line) for line in f.readlines()]
+
+               for d in datas:
+                   cur.execute("insert into peoples(name,sex,age,emotion_state,salary) values('%s','%s',%d,'%s',%d)"%(d[0],d[1],d[2],d[3],d[4]))
+                   conn.commit()
